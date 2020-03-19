522685dac4cb3677e7619457cba793671f7634f5
Konrad <konrad.stochel.13@gmail.com>
2020-03-19 17:34:19 +0100

    develop app by sort tasks

--- src/Components/Task/TaskList/TaskList.js
+++ src/Components/Task/TaskList/TaskList.js
@@ -7,13 +7,24 @@ const TaskList = ({ tasks, deleteTask, doneTask }) => {
   const activeTasks = tasks.filter(item => item.active === true)
   const doneTasks = tasks.filter(item => item.active === false)
 
+  doneTasks.reverse();
+  if (activeTasks.length > 2) {
+    activeTasks.sort((a, b) => {
+      a = a.text.toLowerCase();
+      b = b.text.toLowerCase();
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0
+    })
+  }
+
   const taskActive = activeTasks.map(item => <TaskItem key={item.id} task={item} deleteTask={deleteTask} doneTask={doneTask} />)
   const taskDone = doneTasks.map(item => <TaskItem key={item.id} task={item} deleteTask={deleteTask} doneTask={doneTask} />)
 
   return (
     <>
       <div className="taskList">
-        {taskActive}
+        {taskActive.sort()}
       </div>
       {doneTasks.length > 5 ? <p>Last 5 tasks:</p> : null}
       <div>{taskDone.slice(0, 5)}</div>
