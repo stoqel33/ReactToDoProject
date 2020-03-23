a7939bfc374720fc9a3cb6350d3304f777f5815a
Konrad <konrad.stochel.13@gmail.com>
2020-03-23 16:53:00 +0100

    Updates

---
+++ precache-manifest.aac8dccd1d6af4ac562d04d9d4ef86e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e036d89eef8600ed71cf08c03f7365",
+    "url": "/ReactToDoProject/index.html"
+  },
+  {
+    "revision": "365a75ceca994d1fdbd7",
+    "url": "/ReactToDoProject/static/css/main.a4e004fd.chunk.css"
+  },
+  {
+    "revision": "b5eec637291eb92a1d08",
+    "url": "/ReactToDoProject/static/js/2.4a9af44c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ReactToDoProject/static/js/2.4a9af44c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365a75ceca994d1fdbd7",
+    "url": "/ReactToDoProject/static/js/main.d8df3ada.chunk.js"
+  },
+  {
+    "revision": "9310e93099143e4c1c68",
+    "url": "/ReactToDoProject/static/js/runtime-main.ad3fe82b.js"
+  }
+]);
